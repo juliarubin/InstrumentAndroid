cc89979d77b605ab2a7075c57cc8ac214df5e5be
juliarubin <juliarubin@gmail.com>
2014-11-28 21:51:43 -0500

    now removing unused code

--- src/com/android/drop/features/asm/FilterCallsClassVisitor.java
+++ src/com/android/drop/features/asm/FilterCallsClassVisitor.java
@@ -3,6 +3,7 @@ package com.android.drop.features.asm;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.MethodNode;
 
 import com.android.drop.features.data.ClassHierarchy;
 import com.android.drop.features.data.Constants;
@@ -18,57 +19,60 @@ public class FilterCallsClassVisitor extends BasicClassVisitor {
     public MethodVisitor visitMethod(final int access, final String name,
             final String desc, final String signature,
             final String[] exceptions) {
-		
+			
 		MethodVisitor mv = classWriterVisitor.visitMethod(access, name, desc, signature, exceptions);
-		
+		FilterMethodAdapter methodAdapter = new FilterMethodAdapter(Opcodes.ASM5, mv, this.name, access, name, desc);
 		if ((access & (Opcodes.ACC_ABSTRACT | Opcodes.ACC_NATIVE)) > 0) {
-            return mv;
+            return methodAdapter;
         }
 		
-		//this part instruments all connection requests
-		FilterMethodAdapter methodAdapter = new FilterMethodAdapter(Opcodes.ASM5, mv, this.name, access, name, desc);
-        
-        String methodSigniture = this.name + "." + name +  desc;
-        
-        if (ds.getMethod(methodSigniture) == null) {
-			System.err.println("The method from an app is not found in data structure: " + methodSigniture);	
+		String methodSigniture = this.name + "." + name +  desc;
+		
+		if (ds.getMethod(methodSigniture) == null) {
+			System.err.println("The method from an app is not found in data structure: " + methodSigniture);
+			//leave the method as is
 			return methodAdapter;
 	     }
-	     else if (ds.getMethod(methodSigniture).getCalled() || "<init>".equals(name) || "uncaughtException".equals(name)) {
-//	    	 //debug
-//	    	 AsmUtils.addPrintoutStatement(mv, Constants.FILTER_DEV_LOG_FILE, instrumentationType, "LEFT_CALL " + methodSigniture, 0);
-			return methodAdapter;
+		
+		if (ds.getMethod(methodSigniture).getCalled() || "<init>".equals(name) || "uncaughtException".equals(name)) {
+	    	 //also leave the method as is
+	    	 return methodAdapter;
 	     }
-	     else {
-	    	ClassHierarchy hierarchy = ClassHierarchy.getInstance();
-	    	String printMarker =  Constants.FILTERED_MARKER;
+		
+		//now filter
+		mv.visitCode();
+		
+	    ClassHierarchy hierarchy = ClassHierarchy.getInstance();
+	    String printMarker =  Constants.FILTERED_MARKER;
 	    			    
-		    if (("onClick".equals(name) && "(Landroid/view/View;)V".equals(desc)) ||
-		    	("onFocusChange".equals(name) && "(Landroid/view/View;Z)V".equals(desc)) ||
-		    	("onEditorAction".equals(name) && "(Landroid/widget/TextView;ILandroid/view/KeyEvent;)Z".equals(desc))) {
-		    	AsmUtils.addExcludedPopupForView(mv);
-		    	printMarker = "MODIFIED CALL ";
-		    }
-		    //FIXME walmart search menu
-//		    else if (("onMenuItemSelected".equals(name) && "(ILandroid/view/MenuItem;)Z".equals(desc))) {
+		if (("onClick".equals(name) && "(Landroid/view/View;)V".equals(desc)) ||
+		  	("onFocusChange".equals(name) && "(Landroid/view/View;Z)V".equals(desc)) ||
+		   	("onEditorAction".equals(name) && "(Landroid/widget/TextView;ILandroid/view/KeyEvent;)Z".equals(desc))) {
+		   	AsmUtils.addExcludedPopupForView(mv);
+		   	printMarker = "MODIFIED CALL ";
+	    }
+		 //FIXME walmart search menu
+//		 else if (("onMenuItemSelected".equals(name) && "(ILandroid/view/MenuItem;)Z".equals(desc))) {
 //		    	addExcludedPopupForMenu(mv);
 //		    	printMarker = "MODIFIED CALL ";
-//		    }
-		    else if (("onCreate".equals(name) || "onDestroy".equals(name) || "onPause".equals(name) || "onPostCreate".equals(name) ||
-		    	"onPostResume".equals(name) || "onRestart".equals(name) || "onResume".equals(name) || "onStart".equals(name) ||
-		    	"onStop".equals(name) || "onAttach".equals(name)) && 
-		    	hierarchy.isAncestors(this.name, "android/app/Activity", 0)) {
-		    		    	
-		    	AsmUtils.addCallToSuper(mv, name, desc, this.superName);
-		    }
+//		 }
+		 else if (("onCreate".equals(name) || "onDestroy".equals(name) || "onPause".equals(name) || "onPostCreate".equals(name) ||
+		  	"onPostResume".equals(name) || "onRestart".equals(name) || "onResume".equals(name) || "onStart".equals(name) ||
+	    	"onStop".equals(name) || "onAttach".equals(name)) && 
+	    	hierarchy.isAncestors(this.name, "android/app/Activity", 0)) {		    		    	
+			 AsmUtils.addCallToSuper(mv, name, desc, this.superName);
+			 printMarker = "MODIFIED CALL ";
+		 }
 		    	
-		    AsmUtils.addPrintoutStatement(mv, Constants.FILTER_DEV_LOG_FILE, instrumentationType, printMarker + methodSigniture, 0);
+		AsmUtils.addPrintoutStatement(mv, Constants.FILTER_DEV_LOG_FILE, instrumentationType, printMarker + methodSigniture, 0);
 		    
-			String returnType = desc.substring(desc.lastIndexOf(')')+1, desc.length());
-			AsmUtils.addReturnStatement(mv, returnType);
+	    String returnType = desc.substring(desc.lastIndexOf(')')+1, desc.length());
+		AsmUtils.addReturnStatement(mv, returnType);			
 		
-			return methodAdapter;
-	     }
+		mv.visitMaxs(0, 0);
+		mv.visitEnd();
+	        
+		return null;
         
     }
 	
